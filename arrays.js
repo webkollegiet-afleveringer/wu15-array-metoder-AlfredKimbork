2157c04906225236b761a5013d3a4801eee73500
Alfred K. <107874027+AlfredKimbork@users.noreply.github.com>
2026-02-02 09:27:35 +0100

    shortened Opgave 1

--- arrays.js
+++ arrays.js
@@ -6,40 +6,29 @@ let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
 // --- Løs opgaverne herunder --- 
 
 // -- opgave 1 --
-
-let newNumbers = []
-numbersArray.forEach(num => { if(num > 20) newNumbers.push(num) })
-console.log("Opgave 1", newNumbers)
+console.log("Opgave 1", newNumbers = numbersArray.filter(num => num > 20));
 
 // -- opgave 2 --
-
 catsArray.push("Sniffles")
-
 console.log("Opgave 2", catsArray)
 
 // -- opgave 3 --
-
 console.log("Opgave 3a", dogsArray.includes("Trixie"));
 console.log("Opgave 3b", dogsArray.includes("Baxter"));
 
 // -- opgave 4 --
-
 console.log("Opgave 4a", catsArray.includes("Bagheera") ? catsArray[catsArray.indexOf("Bagheera")] : undefined);
 console.log("Opgave 4b", catsArray.includes("Salem") ? catsArray[catsArray.indexOf("Salem")] : undefined);
 
 // -- opgave 5 --
-
 console.log("Opgave 5", newNumbers = numbersArray.map(num => num * 3));
 
 // -- opgave 6 --
-
 console.log("Opgave 6", dogsArray.join(""));
 
 // -- opgave 7 --
-
 console.log("Opgave 7a", fruitsArray.indexOf("Mango"));
 console.log("Opgave 7a", fruitsArray.indexOf("Blåbær"));
 
 // -- opgave 8 --
-
 console.log("Opgave 8", dogsArray.filter(name => name !== "Polly"))
